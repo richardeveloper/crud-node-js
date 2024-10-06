b670d468938f2da8e7e452ab8fe9502541fa23eb
richardeveloper <richard_ar@hotmail.com>
2024-10-06 07:49:21 -0300

    Ajuste na máscara de telefone celular

--- client/utils/phone-mask.js
+++ client/utils/phone-mask.js
@@ -9,31 +9,19 @@ function applyMaskPhone(event) {
             value = value.replace(/^(\d{2})$/, '($1)');
             break;
         case 3:
-            value = value.replace(/^(\d{2})(\d{1})$/, '($1) $2');
-            break;
         case 4:
-            value = value.replace(/^(\d{2})(\d{2})$/, '($1) $2');
-            break;
         case 5:
-            value = value.replace(/^(\d{2})(\d{3})$/, '($1) $2');
-            break;
         case 6:
-            value = value.replace(/^(\d{2})(\d{4})$/, '($1) $2');
+            value = value.replace(/^(\d{2})(\d{1,4})$/, '($1) $2');
             break;
         case 7:
             value = value.replace(/^(\d{2})(\d{5})$/, '($1) $2-');
             break;
         case 8:
-            value = value.replace(/^(\d{2})(\d{5})(\d{1})$/, '($1) $2-$3');
-            break;
         case 9:
-            value = value.replace(/^(\d{2})(\d{5})(\d{2})$/, '($1) $2-$3');
-            break;
         case 10:
-            value = value.replace(/^(\d{2})(\d{5})(\d{3})$/, '($1) $2-$3');
-            break;
         case 11:
-            value = value.replace(/^(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3');
+            value = value.replace(/^(\d{2})(\d{5})(\d{1,4})$/, '($1) $2-$3');
             break;
         default:
             break;
